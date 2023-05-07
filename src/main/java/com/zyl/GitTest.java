d302059a9ce2518be28767cfc0bf27e3249f2615
zyl <zyl.2001@qq.com>
2023-05-07 16:35:08 +0800

    first commit

---
+++ src/main/java/com/zyl/GitTest.java
@@ -0,0 +1,11 @@
+package com.zyl;
+
+/**
+ * @author 曾杨龙
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
